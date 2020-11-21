a8a06d9621718d2ff18959c5f979690f59e28c68
Nino Ilidan <nino.ilidan@okstate.edu>
2020-11-21 14:04:16 -0600

    Update Training

---
+++ CSharp/Classes, Interfaces and OOP/StackProgram/StackProgram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProgram
+{
+    class Stack {
+
+
+       private LinkedList<object> list = new LinkedList<object>();
+
+      public  void Push(object obj)
+        {
+            list.AddLast(obj);
+        }
+       public object Pop()
+        {
+            object o = list.Last.Value;
+
+            list.RemoveLast();
+
+            return o;
+        }
+
+
+      public  void Clear()
+        {
+            list.Clear();
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
