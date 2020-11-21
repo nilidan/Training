60a1f2a68a35e6b726392b9ed6209e7ca0950bd1
Nino Ilidan <nino.ilidan@okstate.edu>
2020-11-21 00:41:34 -0600

    Traing Update

---
+++ CSharp/Classes, Interfaces and OOP/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+
+        public class Stopwatch
+        {
+            DateTime startTime;
+            Boolean start;
+            DateTime stopTime;
+
+            public void Start()
+            {
+                if (start)
+                {
+                    throw new InvalidOperationException("Cannot start: already running");
+                }
+                Console.WriteLine("\n**Start**\n");
+                startTime = DateTime.Now;
+                start = true;
+            }
+
+            public void Stop()
+            {
+                if (!start)
+                {
+                    throw new InvalidOperationException("Cannot stop: not running");
+                }
+                Console.WriteLine("\n**Stop**\n");
+                start = false;
+                stopTime = DateTime.Now;
+            }
+
+            public TimeSpan ElaspedTime()
+            {
+                if (start)
+                {
+                    return DateTime.Now - startTime;
+                }
+                else
+                {
+                    return stopTime - startTime;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+             var stopwatch = new Stopwatch();
+             int choice;
+             int a;
+             Boolean num;
+
+                do
+                {
+                Console.Write("Stopwatch Program \nEnter your choice below \n");
+                Console.Write(" 1-Start Timer \n 2-Stop Timer \n 3-Read Timer \n 0-Quit \n");
+
+                       
+                num = int.TryParse(Console.ReadLine(),out a);
+
+                if(num)
+                {
+                    choice = a;
+                }
+                else
+                {
+                    choice = 5;
+                }
+                    switch (choice)
+                    {
+                        case 1: stopwatch.Start();
+                                break;
+                        case 2: stopwatch.Stop();
+                                break;
+                        case 3: Console.WriteLine(stopwatch.ElaspedTime());
+                                Console.WriteLine();
+                                break;
+                        default:Console.WriteLine("Invalid Input \n");
+                        break;
+                    }
+
+                } while (choice != 0);
+            
+        }
+    }
+}
---
+++ MachineLearning/Starting Template for Trainging and Splitting/data_preprocessing_template.py
@@ -0,0 +1,23 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3]=imputer.transform(X[:, 1:3])
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
