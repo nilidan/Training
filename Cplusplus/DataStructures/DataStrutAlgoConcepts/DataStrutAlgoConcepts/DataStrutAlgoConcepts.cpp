330d98e8dbc72aa2d54acb58b25f94fca3057234
Nino Ilidan <nino.ilidan@okstate.edu>
2020-12-01 16:23:37 -0600

    Merge Sort

--- Cplusplus/DataStructures/DataStrutAlgoConcepts/DataStrutAlgoConcepts/DataStrutAlgoConcepts.cpp
+++ Cplusplus/DataStructures/DataStrutAlgoConcepts/DataStrutAlgoConcepts/DataStrutAlgoConcepts.cpp
@@ -19,7 +19,18 @@ int main()
 	//printNum(5);
 	//cout<<multiply(3, 5);
 	//cout<<countZero(10320);
-	cout << geoSum(3);
+	//cout << geoSum(3);
+
+	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Given array is \n";
+	printArray(arr, arr_size);
+
+	mergeSort(arr, 0, arr_size - 1);
+
+	cout << "\nSorted array is \n";
+	printArray(arr, arr_size);
 	return 0;
 }
 
--- Cplusplus/DataStructures/DataStrutAlgoConcepts/DataStrutAlgoConcepts/recursion.h
+++ Cplusplus/DataStructures/DataStrutAlgoConcepts/DataStrutAlgoConcepts/recursion.h
@@ -150,3 +150,78 @@ double geoSum(int k)
 	return smallAns + 1.0 / pow(2, k);
 }
 
+void merge(int arr[], int l, int m, int r) {
+
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	// Create temp arrays
+	int *L = new int [n2];
+	int *R = new int [n1];
+
+	// Copy data to temp arrays L[] and R[]
+	for (int i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	// Merge the temp arrays back into arr[l..r]
+
+	// Initial index of first subarray
+	int i = 0;
+
+	// Initial index of second subarray
+	int j = 0;
+
+	// Initial index of merged subarray
+	int k = l;
+
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// L[], if there are any
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	// Copy the remaining elements of
+	// R[], if there are any
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(int a[], int s, int e) {
+	if (s >= e) {
+		return;
+	}
+
+	int mid = (s + e ) / 2;
+	mergeSort(a, s, mid);
+	mergeSort(a, mid+1, e);
+	merge(a, s, mid, e);
+
+
+
+
+}
+
+void printArray(int A[], int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << A[i] << " ";
+}
