d228a9c0b9a666e8ecd61ce12f109d8e30d08a18
Nino Ilidan <nino.ilidan@okstate.edu>
2021-04-16 14:20:15 -0500

    Deleted Field

--- DesignPattersCplusplus/DesignPatternFactories/DesignPatternFactories/DesignPatternFactories.cpp
+++ DesignPattersCplusplus/DesignPatternFactories/DesignPatternFactories/DesignPatternFactories.cpp
@@ -1,4 +1,4 @@
-// DesignPatternFields.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// DesignPatternFactories.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
